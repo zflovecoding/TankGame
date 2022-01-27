a401df9af12a206fefaa266999620f5adbde70fb
zflovcoding <2043166212@qq.com>
2022-01-27 16:21:20 +0800

    update record

--- src/com/geekpig/tankgame5/Recorder.java
+++ src/com/geekpig/tankgame5/Recorder.java
@@ -4,17 +4,30 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Vector;
 
 public class Recorder {
     private static  int  allEnemyTankNum = 0 ;
     private static FileWriter fw = null;
     private static BufferedWriter bw = null;
     private static String filePath =  "src\\record.txt";
+    private static Vector<EnemyTank> enemyTanks = new Vector<>();
+
+    public static  void setEnemyTanks(Vector<EnemyTank> enemyTanks) {
+        Recorder.enemyTanks = enemyTanks;
+    }
 
     public static void keepRecord(){
+
         try {
             bw = new BufferedWriter(new FileWriter(filePath));
             bw.write(allEnemyTankNum+"\r\n");
+            for (int i = 0; i < enemyTanks.size(); i++) {
+                EnemyTank enemyTank = enemyTanks.get(i);
+                String location =  enemyTank.getX() +" "+ enemyTank.getY()+" "+enemyTank.getDirect()+" ";
+                bw.write(location+"\r\n");
+
+            }
 
 
         } catch (IOException e) {
